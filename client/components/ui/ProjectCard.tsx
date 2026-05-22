6ca34db596a61c3175f61863afa1b6cf19b2f7e2
Devon-Hill7 <devon17@ksu.edu>
2026-05-22 18:10:07 -0500

    Cleaned up ProjectCard file

--- client/components/ui/ProjectCard.tsx
+++ client/components/ui/ProjectCard.tsx
@@ -13,30 +13,6 @@ interface ProjectCardProps {
 }
 
 const ProjectCard = ({ title, description, image, techStack, repoUrl, liveUrl }: ProjectCardProps) => {
-  if(liveUrl) {
-    return (
-        <Card>
-            <CardHeader>
-                <CardTitle >{title}</CardTitle>
-                <img
-                src={image}
-                className="w-full h-48 object-cover rounded-md"
-                />
-                <CardDescription>{description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-                {techStack.map((tech) => (
-                    <Badge key={tech} variant="outline">{tech}</Badge>
-                ))}
-            </CardContent>
-            <CardFooter>
-                <Button>View Repo</Button>
-                <Button variant="outline">View Live</Button>
-            </CardFooter>
-        </Card>
-    )
-  }
-
     return (
         <div className="w-full h-full rounded-2xl p-5 bg-white/10 backdrop-blur-xl border border-white/10 shadow-[0_8px_32px_rgba(0,0,0,0.35),inset_0_1px_1px_rgba(255,255,255,0.2)]">
             <Card className="w-full gap-5">
@@ -60,7 +36,13 @@ const ProjectCard = ({ title, description, image, techStack, repoUrl, liveUrl }:
                         <Button size="lg" className="text-xl px-4 py-2 rounded-lg bg-orange shadow-[-0.45px_-0.75px_1px_rgba(255,185,140,0.6),0.45px_8px_8px_rgba(0,13,18,0.6)]
                              hover:-translate-y-1 hover:scale-[1.04] hover:shadow-[[-0.45px_-0.75px_1px_rgba(255,185,140,0.6), 0_16px_40px_rgba(0,0,0,0.28)]">
                                 View Repo
-                        </Button>   
+                        </Button> 
+                        {liveUrl && (
+                            <Button size="lg" className="text-xl px-4 py-2 rounded-lg bg-green shadow-[-0.45px_-0.75px_1px_rgba(144,238,144,0.6),0.45px_8px_8px_rgba(0,13,18,0.6)]
+                             hover:-translate-y-1 hover:scale-[1.04] hover:shadow-[[-0.45px_-0.75px_1px_rgba(144,238,144,0.6), 0_16px_40px_rgba(0,0,0,0.28)]">
+                                View Live
+                            </Button>
+                        )}
                     </div>
                 </CardFooter>
             </Card>
