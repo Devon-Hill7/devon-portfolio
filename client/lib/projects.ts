da7647a8e904aa821ee7c6f9f65c5d904549584b
Devon-Hill7 <devon17@ksu.edu>
2026-06-22 16:35:48 -0500

    Changed the photo for MySpanishDiary Project and updated the repo link for the portfolio project card

--- client/lib/projects.ts
+++ client/lib/projects.ts
@@ -15,7 +15,7 @@ export const projects: Project[] = [
     title: "My Spanish Diary",
     description:
       "Full-stack journaling app with grammar feedback and secure user authentication",
-    image: "/images/MySpanishDiaryScreenshot.png",
+    image: "/images/MySpanishDiaryHomeScreen.png",
     techStack: ["Spring Boot", "React", "PostgreSQL", "Java", "JWT"],
     repoUrl: "https://github.com/Devon-Hill7/MySpanishDiary",
   },
@@ -26,7 +26,7 @@ export const projects: Project[] = [
       "Full-stack journaling app with grammar feedback and secure user authentication",
     image: "/images/MySpanishDiaryScreenshot.png",
     techStack: ["Next.js", "TypeScript", "TailWindCSS"],
-    repoUrl: "https://github.com/Devon-Hill7/MySpanishDiary",
+    repoUrl: "https://github.com/Devon-Hill7/devon-portfolio",
   }
   
 
